9f414edc0a6415a80f64343ef395bc2b7847b521
Ant0yne <antoine.soliv@gmail.com>
2024-03-24 09:03:39 +0100

    exercise-component

--- src/components/Exercise/index.jsx
+++ src/components/Exercise/index.jsx
@@ -1,10 +1,15 @@
 import "./exercise.css";
 
 const Exercise = ({ exercise }) => {
+	const { id, title, website, repository, img, description } = exercise;
+
 	return (
 		<>
-			<nav className="exercise">
-				<p>test</p>
+			<nav className="exercise" style={{ backgroundImage: `url(${img})` }}>
+				<h3>{title}</h3>
+				<a href={website}> Website</a>
+				<a href={repository}> Website</a>
+				<p>{description}</p>
 			</nav>
 		</>
 	);
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,8 @@ const Header = () => {
 	return (
 		<>
 			<header>
-				<h1>Todo List</h1>
+				<h1>React Exercises</h1>
+				<h2>A selection of my React exercises</h2>
 			</header>
 		</>
 	);
